36dcd450d68728f6e67702f54677ba2871686877
araka1986 <arata.kamijo@gmail.com>
2017-11-24 23:24:22 +0900

    modified by arata

--- src/jp/jojo/basics/Player.java
+++ src/jp/jojo/basics/Player.java
@@ -6,8 +6,8 @@ class Player {
 	int speed;
 	int dribbling;
 	int power;
-
-	Player(String name, int speed, int dribbling, int power) {
+	
+	Player(final String name, final int speed, final int dribbling, final int power) {
 		this.name = name;
 		this.speed = speed;
 		this.dribbling = dribbling;
