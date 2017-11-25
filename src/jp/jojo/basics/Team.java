c1fa5647ddc2f11714d5cd419be4926b41af95e5
Srdan Maksimovic <srdan.maksimovic@hashcode.hr>
2017-11-25 10:23:05 +0100

    Create sketch of class Team

---
+++ src/jp/jojo/basics/Team.java
@@ -0,0 +1,15 @@
+package jp.jojo.basics;
+
+import java.util.List;
+
+class Team {
+
+	// this class models a team (consisting of group of players)
+	// first task is to finish this class (create fields and fill them in constructor) 
+	// and publish the number of players to the outer world
+
+	Team(List<Player> players) {
+
+	}
+
+}
